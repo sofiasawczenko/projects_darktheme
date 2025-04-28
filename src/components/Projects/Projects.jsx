e26f89ed4aa873949df5143c1251b34fa406f5bc
Sofia Sawczenko <ra104242@uem.br>
2025-04-28 10:45:47 -0300

    description without marks

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -64,9 +64,9 @@ const Projects = () => {
               </div>
             </div>
             <div class="card-body">
-              <h5 class="card-title">Previsao de Insuficiencia Cardiaca com Aprendizado Supervisidionado</h5>
+              <h5 class="card-title">Previsao de Insuficiencia Cardiaca com Aprendizado Supervisionado</h5>
               <p class="card-text">
-              Projeto de predição de insuficiência cardíaca usando um conjunto de dados de Kaggle, com técnicas de pré-processamento de dados e validação com regressão logística.
+              Projeto de predicao de insuficiencia cardiaca usando um conjunto de dados de Kaggle, com tecnicas de pre-processamento de dados e validacao com regressao logistica.
               </p>
             </div>
             <div class="card-footer">Python, Regressao Logistica</div>
@@ -83,9 +83,9 @@ const Projects = () => {
               </div>
             </div>
             <div class="card-body">
-              <h5 class="card-title">Previsão do Clima com Aprendizado Supervisidionado</h5>
+              <h5 class="card-title">Previsao do Clima com Aprendizado Supervisionado</h5>
               <p class="card-text">
-              Previsão de condições climáticas utilizando técnicas de regressão linear e ridge regression com base em dados históricos de clima de Nova York.
+              Previsao de condicoes climaticas utilizando tecnicas de regressao linear e ridge regression com base em dados historicos de clima de Nova York.
               </p>
             </div>
             <div class="card-footer">Python, Regressao Linear</div>
@@ -99,13 +99,12 @@ const Projects = () => {
                 <a href="https://github.com/sofiasawczenko/forecasting_house_price">
                   <FiGithub />
                 </a>
-                
               </div>
             </div>
             <div class="card-body">
-              <h5 class="card-title">Previsão do Preço de Imóveis com Aprendizado Supervisidionado</h5>
+              <h5 class="card-title">Previsao do Preco de Imoveis com Aprendizado Supervisionado</h5>
               <p class="card-text">
-              Desenvolvimento de um modelo de machine learning para prever o preço médio dos imóveis em Boston, utilizando análise de dados e técnicas de regressão.
+              Desenvolvimento de um modelo de machine learning para prever o preco medio dos imoveis em Boston, utilizando analise de dados e tecnicas de regressao.
               </p>
             </div>
             <div class="card-footer">Python, Regressao polinomial</div>
@@ -122,9 +121,9 @@ const Projects = () => {
               </div>
             </div>
             <div class="card-body">
-              <h5 class="card-title">Análise de Tendências de Vendas com Previsão de Séries Temporais</h5>
+              <h5 class="card-title">Analise de Tendencias de Vendas com Previsao de Series Temporais</h5>
               <p class="card-text">
-              Análise de dados de vendas para identificar tendências e realizar previsões com métodos de séries temporais utilizando Python e bibliotecas como pandas e seaborn.
+              Analise de dados de vendas para identificar tendencias e realizar previsoes com metodos de series temporais utilizando Python e bibliotecas como pandas e seaborn.
               </p>
             </div>
             <div class="card-footer">Pandas, Seaborn, Matplotlib</div>
@@ -143,7 +142,7 @@ const Projects = () => {
             <div class="card-body">
               <h5 class="card-title">Pipeline ETL de Dados Sensiveis de Funcionarios</h5>
               <p class="card-text">
-              Pipeline ETL para processar dados sensíveis de funcionários usando Google Cloud Data Fusion, Cloud Composer, e BigQuery, com foco em eficiência e segurança.
+              Pipeline ETL para processar dados sensiveis de funcionarios usando Google Cloud Data Fusion, Cloud Composer, e BigQuery, com foco em eficiencia e seguranca.
               </p>
             </div>
             <div class="card-footer">Python, GCP, Google Data Fusion</div>
@@ -162,7 +161,7 @@ const Projects = () => {
             <div class="card-body">
               <h5 class="card-title">Pipeline ETL de Estatisticas de Jogos</h5>
               <p class="card-text">
-              Pipeline para processar e analisar estatísticas de jogos de críquete utilizando serviços do Google Cloud (GCP), como Cloud Functions e Cloud Dataflow.
+              Pipeline para processar e analisar estatisticas de jogos de criquete utilizando servicos do Google Cloud (GCP), como Cloud Functions e Cloud Dataflow.
               </p>
             </div>
             <div class="card-footer">Python, GCP, Cloud Functions</div>
@@ -181,7 +180,7 @@ const Projects = () => {
             <div class="card-body">
               <h5 class="card-title">Sudoku Solver</h5>
               <p class="card-text">
-              Back-end em Java para resolver um quebra-cabeça de Sudoku utilizando o algoritmo de retrocesso (backtracking), oferecendo uma solução eficiente para o problema.
+              Back-end em Java para resolver um quebra-cabeca de Sudoku utilizando o algoritmo de retrocesso (backtracking), oferecendo uma solucao eficiente para o problema.
               </p>
             </div>
             <div class="card-footer">Java, Algoritmo de BackTracking</div>
@@ -200,7 +199,7 @@ const Projects = () => {
             <div class="card-body">
               <h5 class="card-title">Web Scraping de Produtos de Marketplace com Python</h5>
               <p class="card-text">
-              Scraping de dados de produtos de marketplaces usando Python, BeautifulSoup e Requests, com armazenamento em banco de dados SQLite para análise posterior.
+              Scraping de dados de produtos de marketplaces usando Python, BeautifulSoup e Requests, com armazenamento em banco de dados SQLite para analise posterior.
               </p>
             </div>
             <div class="card-footer">Python, BeautifulSoup, Requests</div>
@@ -217,9 +216,9 @@ const Projects = () => {
               </div>
             </div>
             <div class="card-body">
-              <h5 class="card-title">Análise de Sentimento em Dados Textuais</h5>
+              <h5 class="card-title">Analise de Sentimento em Dados Textuais</h5>
               <p class="card-text">
-              Projeto que analisa o sentimento de dados textuais, usando bibliotecas como NLTK, TextBlob e Newspaper3k para processamento e análise de artigos e textos.
+              Projeto que analisa o sentimento de dados textuais, usando bibliotecas como NLTK, TextBlob e Newspaper3k para processamento e analise de artigos e textos.
               </p>
             </div>
             <div class="card-footer">NLTK, TextBlob, Newspaper3k</div>
@@ -236,12 +235,12 @@ const Projects = () => {
               </div>
             </div>
             <div class="card-body">
-              <h5 class="card-title">Assistente Virtual na Extensão do Navegador</h5>
+              <h5 class="card-title">Assistente Virtual na Extensao do Navegador</h5>
               <p class="card-text">
-              Assistente virtual com IA integrado ao navegador para oferecer assistência personalizada ao usuário em tempo real, utilizando a API do Google Gemini.
+              Assistente virtual com IA integrado ao navegador para oferecer assistencia personalizada ao usuario em tempo real, utilizando a API do Google Gemini.
               </p>
             </div>
-            <div class="card-footer">Java, Gemini API, REACT.js </div>
+            <div class="card-footer">Java, Gemini API, REACT.js</div>
           </div>
         </div>
       </div>
